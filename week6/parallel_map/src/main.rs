4e602fe826ab027145a0b4a9daf3ed894b2f75d2
Ray <zhuoxun.yang777@outlook.com>
2023-07-12 17:45:28 +0800

    week 6 finished

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,37 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    output_vec.resize_with(input_vec.len(), Default::default);
+
+    let (input_sender, input_receiver) = crossbeam_channel::unbounded::<(usize, T)>();
+    let (output_sender, output_receiver) = crossbeam_channel::unbounded::<(usize, U)>();
+    let mut handles = vec![];
+
+    while let Some(input) = input_vec.pop() {
+        input_sender.send((input_vec.len(), input)).expect("wrong input sending");
+    }
+    drop(input_sender);
+
+    for _ in 0..num_threads {
+        let c_output_sender = output_sender.clone();
+        let c_input_receiver = input_receiver.clone();
+        let handle = thread::spawn(move || {
+            while let Ok((counter, input)) = c_input_receiver.recv() {
+                let res = f(input);
+                c_output_sender.send((counter, res)).expect("wrong output sending");
+            }
+            drop(c_output_sender);
+        });
+        handles.push(handle);
+    }
+    drop(output_sender);
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    while let Ok((index, output)) = output_receiver.recv() {
+        output_vec[index] = output;
+    }
     output_vec
 }
 
